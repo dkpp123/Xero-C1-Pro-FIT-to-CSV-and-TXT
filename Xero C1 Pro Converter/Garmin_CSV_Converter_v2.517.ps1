2e60070da3187d972f977b32344671c8e1760091
dkpp123 <user@email.com>
2024-01-22 12:59:36 -0800

    CLEANUP

--- Xero C1 Pro Converter/Garmin_CSV_Converter_v2.517.ps1
+++ Xero C1 Pro Converter/Garmin_CSV_Converter_v2.517.ps1
@@ -23,17 +23,16 @@ remove-item -path .\fit_temp\*.fit -ErrorAction SilentlyContinue
 $Fit = New-Object System.Windows.Forms.OpenFileDialog -Property @{ 
     InitialDirectory = [Environment]::GetFolderPath('Desktop') 
     Filter = 'Garmin fit (*.fit)|*.fit'
-    Multiselect = $true
-}
+    Multiselect = $true}
 $null = $Fit.ShowDialog()
 
 # copies to the "fit_temp" folder first for processing.  Does not interact with device except to copy the files to computer first.
 if (!$fit.filenames) 
-{
-    Write-Output "No Files Selectd `nExiting"
-    pause
-    break
-}
+    {
+        Write-Output "No Files Selectd `nExiting"
+        pause
+        break
+    }
 
 Copy-Item $fit.filenames .\fit_Temp -ErrorAction SilentlyContinue
 
@@ -42,8 +41,8 @@ Copy-Item $fit.filenames .\fit_Temp -ErrorAction SilentlyContinue
 $fit_temp = get-childitem -Path .\fit_temp\*.fit 
 foreach ($fitfile in $fit_temp)
     {
-    $fit2 = "`"$fitfile`""
-    start-process -filepath '.\java\fittocsv.bat' -argumentlist $fit2 -wait
+        $fit2 = "`"$fitfile`""
+        start-process -filepath '.\java\fittocsv.bat' -argumentlist $fit2 -wait
     }
 
 # gets list of newly created .csv from fit_temp directory for processing
@@ -55,54 +54,50 @@ foreach ($item in $items)
         $csv1 = $item.fullname | import-csv
         $csv_data = $csv1 | where-object 'type' -eq 'data' | where-object 'local number' -eq 4
         $measured = $csv_data.'value 2' | measure-object -average
-        $newNumbers = 0
+        $mean = $null
         ForEach ($number in $csv_data) 
-            {
-            $newNumbers += [Math]::Pow(($number.'value 2' - $measured.Average), 2)
-            }
-        $stdDev = [math]::Sqrt($($newNumbers / ($measured.Count - 1)))
-        $stdDevp = [math]::Sqrt($($newNumbers / ($measured.Count)))
+            {$mean += [Math]::Pow(($number.'value 2' - $measured.Average), 2)}
+        $stdDev = [math]::Sqrt($($mean / ($measured.Count - 1)))
+        $stdDevp = [math]::Sqrt($($mean / ($measured.Count)))
         $stddev_fps=$stddev / 304.79999025
         $stddevp_fps=$stddevp / 304.79999025
 
 # Begin CSV to TXT file processing
 # csv location detection for general statistics and bullet weight
-    foreach ($csv in $csv1)
-        {
-            $type = $csv.type
-		    $localnumber = $csv."local number"
+        foreach ($csv in $csv1)
+            {
+                $type = $csv.type
+		        $localnumber = $csv."local number"
 
 # value correction to feet per second, local time, bullet weight 
-            if ($type -eq "Data" -and $localnumber -eq "3")
-            {
-                $min = ($csv."Value 2" / 304.79999025)
-                $max = ($csv."Value 3" / 304.79999025)
-		        $average = ($csv."Value 4" / 304.79999025)
-		        $shots = $csv."Value 6"
-                $time_hack=([int]$csv."Value 1" + [int]631065600)
-                $time = (([System.DateTimeOffset]::FromUnixTimeSeconds($time_hack)).DateTime.ToLocalTime()).ToString("s")
-		        $extreme_spread= ($max - $min)
-                $weight = ($csv."Value 5" / 10)
-                $tdelta = $null                
-                $kenergya = (([Math]::Pow($average ,2) * $weight) / 450437)
+                if ($type -eq "Data" -and $localnumber -eq "3")
+                    {
+                        $min = ($csv."Value 2" / 304.79999025)
+                        $max = ($csv."Value 3" / 304.79999025)
+		                $average = ($csv."Value 4" / 304.79999025)
+		                $shots = $csv."Value 6"
+                        $time_hack=([int]$csv."Value 1" + [int]631065600)
+                        $time = (([System.DateTimeOffset]::FromUnixTimeSeconds($time_hack)).DateTime.ToLocalTime()).ToString("s")
+		                $extreme_spread= ($max - $min)
+                        $weight = ($csv."Value 5" / 10)
+                        $tdelta = $null                
+                        $kenergya = (([Math]::Pow($average ,2) * $weight) / 450437)
                 
 # sessionn info, user prompt for session name. if empty will still process using file name as base           
-                $Session_name = Read-Host -Prompt "Session Time/Date: $time `nNum of Shots $shots `nBullet Weight gr: $weight `n `nMin: $min `nMax: $max `nAverage: $average `n `nStandard Deviation: $stddev_fps `nPopulation Standard Deviation: $stddevp_fps `nExtreme Spread: $extreme_spread `nAverage Kinetic Energy: $kenergya `n `n `nEnter Session Name"
-                if (!$Session_name) 
-                    {
-                        $Session_name = $item.Name.Replace('.', '_')
-                    }
-                    $filecheck = get-childitem -Path .\Results\$session_name.txt 
-                if ($Session_name = $filecheck.name)
-                    {
-                        $Session_name = -join($session_name,"_new")
-                    }
+                        $Session_name = Read-Host -Prompt "Session Time/Date: $time `nNum of Shots $shots `nBullet Weight gr: $weight `n `nMin: $min `nMax: $max `nAverage: $average `n `nStandard Deviation: $stddev_fps `nPopulation Standard Deviation: $stddevp_fps `nExtreme Spread: $extreme_spread `nAverage Kinetic Energy: $kenergya `n `n `nEnter Session Name"
+                        
+                        if (!$Session_name) 
+                            {$Session_name = $item.Name.Replace('.', '_')                            }
+                        
+                        $filecheck = get-childitem -Path .\Results\$session_name.txt 
+                        if ($Session_name = $filecheck.name)
+                            {$Session_name = -join($session_name,"_new")}
 
 # Write TXT file and dummy CSV for later processing into a real CSV              
-                write-output "Session Time/Date: $time `nSession Name: $session_name `nNum of Shots: $shots `nBullet Weight: $weight `n `nMin: $min `nMax: $Max 'nStandard Deviation: $stddev_fps `nPopulation Standard Deviation: $stddevp_fps 'nExtreme Spread: $extreme_spread  `nAverage Kinetic Energy FPE: $kenergya `n" | out-file -filepath .\Results\$Session_name.txt -Append
-                Write-output '"Shot #","Speed (FPS)","Delta from AVG (FPS)","Delta from Last (FPS)","Kinetic Energy (Ft-LBS)","Power Factor (kgr-ft/s)", "Time"' | out-file -filepath .\Results\$Session_name.csx -Append
-                clear-host
-                }
+                        write-output "Session Time/Date: $time `nSession Name: $session_name `nNum of Shots: $shots `nBullet Weight: $weight `n `nMin: $min `nMax: $Max 'nStandard Deviation: $stddev_fps `nPopulation Standard Deviation: $stddevp_fps 'nExtreme Spread: $extreme_spread  `nAverage Kinetic Energy FPE: $kenergya `n" | out-file -filepath .\Results\$Session_name.txt -Append
+                        Write-output '"Shot #","Speed (FPS)","Delta from AVG (FPS)","Delta from Last (FPS)","Kinetic Energy (Ft-LBS)","Power Factor (kgr-ft/s)", "Time"' | out-file -filepath .\Results\$Session_name.csx -Append
+                        clear-host
+                    }
         
 # csv location detection for individual shot data and value processing               
                 if ($type -eq "Data" -and $localnumber -eq "4")
